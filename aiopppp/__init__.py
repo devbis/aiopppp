0ed8f63a1c896a9df3885d2566d4276c1d3175dc
Ivan Belokobylskiy <belokobylskij@gmail.com>
2025-03-05 13:37:46 +0400

    use dynamic version in pyproject.toml

--- aiopppp/__init__.py
+++ aiopppp/__init__.py
@@ -1,3 +1,4 @@
+from .__version__ import __version__  # noqa: F401
 from .device import Device, find_device  # noqa: F401
 from .discover import Discovery  # noqa: F401
 from .exceptions import (  # noqa: F401
